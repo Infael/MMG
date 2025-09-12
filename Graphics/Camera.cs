97c1eab7980b867233ad1cd637267810008a73fc
Michal Štefaňák <michalstefanak22@gmail.com>
2025-09-12 09:59:38 +0200

    feat: add camera class

---
+++ Graphics/Camera.cs
@@ -0,0 +1,27 @@
+using Microsoft.Xna.Framework;
+
+namespace MMG.Graphics;
+
+public class Camera(int viewportWidth, int viewportHeight)
+{
+  public Vector2 Position { get; set; } = Vector2.Zero;
+  public float Zoom { get; set; } = 1.0f;
+  public float Rotation { get; set; } = 0.0f;
+
+  public int ViewportWidth = viewportWidth;
+  public int ViewportHeight = viewportHeight;
+
+  public Matrix GetViewMatrix()
+  {
+    return
+        Matrix.CreateTranslation(new Vector3(-Position.X, -Position.Y, 0)) *
+        Matrix.CreateRotationZ(Rotation) *
+        Matrix.CreateScale(Zoom) *
+        Matrix.CreateTranslation(new Vector3(ViewportWidth / 2f, ViewportHeight / 2f, 0));
+  }
+
+  public void FollowPosition(Vector2 playerPosition, float smoothing = 0.1f)
+  {
+    Position = Vector2.Lerp(Position, playerPosition, smoothing);
+  }
+}
